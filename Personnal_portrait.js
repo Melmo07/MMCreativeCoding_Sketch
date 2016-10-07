7e9362920363d882938542e5bad85dc710510e27
Mélissa Monnier <MelissaMonnier@melissa-monnier.local>
2016-10-07 14:16:50 +0200

    IgotEyes

--- Personnal_portrait.js
+++ Personnal_portrait.js
@@ -10,23 +10,24 @@ function setup() {
   //print ("Hello World");
   createCanvas(1000,500);
   background(55,155,155);
-  frameRate(7);
+  frameRate(25);
+  stroke(50);
 }
 
 function draw() {
   print(counter + " : " + counter2);
   // rectangle (x,y,width, height)
   //line (x1, y1, x2, y2)
+  
+  line(0,height,width,0);
 fill(random(250), random(250), random(250));
   // ellipse (x,y,width,height)
-  ellipse(width/2, height/2+d, d, d*2);
   ellipse(width/2,height/2,d,d);
   ellipse(width/2+50, height/2-100, d/2,d/2);
   ellipse(width/2-50, height/2-100, d/2,d/2);
-  ellipse(width/2+50, height/2, d/4,d/4);
-  ellipse(width/2-50, height/2, d/4,d/4);
 // triangle(x1y1, x2y2, x3y3)
-
+triangle((100,150,400);
+  fill(0,0,0);
 
 
 

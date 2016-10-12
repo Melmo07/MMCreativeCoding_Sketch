ac950f43c78c51287baa8c8f0bdf8fea4741171f
Mélissa Monnier <MelissaMonnier@melissa-monnier.local>
2016-10-12 10:51:59 +0200

    ranger

---
+++ Dots_Bug/sketch.js
@@ -0,0 +1,59 @@
+var xPos = 0;
+var yPos = 0;
+var d = 60
+var width = 788/9;
+var height = 592;
+
+function setup() {
+  //print ("Hello World");
+  createCanvas(788,592);
+  background(200,200,200);
+  frameRate(25);
+}
+
+function draw() {
+  ellipse(width-100, height-100, d,d);
+  ellipse(width-200, height-100, d,d);
+  ellipse(width-300, height-100, d,d);
+  ellipse(width-400, height-100, d,d);
+  ellipse(width-500, height-100, d,d);
+  ellipse(width-600, height-100, d,d);
+  ellipse(width-700, height-100, d,d);
+  ellipse(width-100, height-200, d,d);
+  ellipse(width-200, height-200, d,d);
+  ellipse(width-300, height-200, d,d);
+  ellipse(width-400, height-200, d,d);
+  ellipse(width-500, height-200, d,d);
+  ellipse(width-600, height-200, d,d);
+  ellipse(width-700, height-200, d,d);
+  ellipse(width-100, height-300, d,d);
+  ellipse(width-200, height-300, d,d);
+  ellipse(width-300, height-300, d,d);
+  ellipse(width-400, height-300, d,d);
+  ellipse(width-500, height-300, d,d);
+  ellipse(width-600, height-300, d,d);
+  ellipse(width-700, height-300, d,d);
+  ellipse(width-100, height-400, d,d);
+  ellipse(width-200, height-400, d,d);
+  ellipse(width-300, height-400, d,d);
+  ellipse(width-400, height-400, d,d);
+  ellipse(width-500, height-400, d,d);
+  ellipse(width-600, height-400, d,d);
+  ellipse(width-700, height-400, d,d);
+  ellipse(width-100, height-500, d,d);
+  ellipse(width-200, height-500, d,d);
+  ellipse(width-300, height-500, d,d);
+  ellipse(width-400, height-500, d,d);
+  ellipse(width-500, height-500, d,d);
+  ellipse(width-600, height-500, d,d);
+  ellipse(width-700, height-500, d,d);
+  fill(150);
+  stroke(150);
+
+
+
+
+
+
+
+}
